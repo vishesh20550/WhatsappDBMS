36e326ddeecd729abbd3bbd20f0482baf86518d3
ashishkamathi <ashishkamathi@rocketmail.com>
2022-04-22 16:26:45 +0530

    Added xml for group chat and chat

---
+++ app/src/main/java/com/example/whatsappdbms/Personal_Chats.java
@@ -0,0 +1,14 @@
+package com.example.whatsappdbms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Personal_Chats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_chats);
+    }
+}
