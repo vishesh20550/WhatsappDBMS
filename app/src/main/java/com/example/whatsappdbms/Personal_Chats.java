07e201d205b7f282bc4258d43ca61d2f5873f6ec
Vishesh Jain <vishesh20550@iiitd.ac.in>
2022-04-26 17:20:50 +0530

    new commit

--- app/src/main/java/com/example/whatsappdbms/Personal_Chats.java
+++ app/src/main/java/com/example/whatsappdbms/Personal_Chats.java
@@ -17,7 +17,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.concurrent.CompletableFuture;
 
 public class Personal_Chats extends AppCompatActivity {
     ArrayList<String> message;
@@ -25,36 +24,16 @@ public class Personal_Chats extends AppCompatActivity {
    HashMap<String,String> userid_name;
    Chat_Adapter chat_adapter;
    ListView listView;
-   String id;
-   String type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_chats);
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            id = extras.getString("id");
-            type=extras.getString("type");
-
-        }
         message=new ArrayList<>();
         message_sender=new ArrayList<>();
         userid_name = new HashMap<>();
         listView=findViewById(R.id.chatlist);
-
-        if(type.equals("group")){
-            CompletableFuture.runAsync(() -> {
-                // method call or code to be asynch.
-                get_group_data();
-
-            });
-            setAdapter();
-
-        }else{
-            getData();
-        }
-
+        getData();
 
     }
 
@@ -83,98 +62,38 @@ public class Personal_Chats extends AppCompatActivity {
                 userid_name.put("1",output.getString(1));
 
             }
-             sql="select name from userlist where userid ="+id+";";
+             sql="select name from userlist where userid = 2;";
              output=  stmt.executeQuery(sql);
             while(output.next()){
-                userid_name.put(id,output.getString(1));
+                userid_name.put("2",output.getString(1));
 
             }
 
-            sql = "Select * from chats where (senderid=1 and recieverid="+id+") or (senderid="+id+" and recieverid=1) order by senttime;";
+            sql = "Select * from chats where (senderid=1 and recieverid=2) or (senderid=2 and recieverid=1) order by senttime;";
              output=  stmt.executeQuery(sql);
 
             while(output.next()){
                 Log.e("Output",output.getString(1));
                 Log.e("Output",output.getString(2));
-               message.add(output.getString(2));
+                message.add(output.getString(2));
                 Log.e("Output",output.getString(3));
                 Log.e("Output",output.getString(4));
-               // Log.e("Output",output.getString(5));
+                Log.e("Output",output.getString(5));
                 message_sender.add(userid_name.get(output.getString(5)));
-              //  Log.e("Output",output.getString(6));
+                Log.e("Output",output.getString(6));
+
                // names.add(output.getString(1));
             }
             setAdapter();
+
             connection.close();
-            setAdapter();
+            //setAdapter();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
             Log.e("Error",throwables.toString());
         }
 
     }
-    public void get_group_data(){
-        String username = "root";
-        String password = "DBMSProject123";
-        Connection connection= null;
-
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-
-        StrictMode.setThreadPolicy(policy);
-        try {
-            Class.forName("net.sourceforge.jtds.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        String ConnectionURL= "jdbc:mysql://103.252.116.130:3306/Whatsapp";
-        try {
-            connection= DriverManager.getConnection(ConnectionURL,username,password);
-            Log.e("Success","Connection Successful");
-            Statement stmt = connection.createStatement();
-            String sql="Select senderid from groupchats where groupmessageid in ( select groupmessageid from groupmessageid where grouplistid="+id+");";
-            ResultSet output=  stmt.executeQuery(sql);
-            while(output.next()){
-                Statement stmt1 = connection.createStatement();
-                sql="select name from userlist where userid ="+output.getString(1)+";";
-                ResultSet output1=  stmt1.executeQuery(sql);
-                while(output1.next()) {
-                    userid_name.put(output.getString(1), output1.getString(1));
-                }
-            }
-
-            sql = "Select * from groupchats where groupmessageid in ( select groupmessageid from groupmessageid where grouplistid="+id+");";
-            output=  stmt.executeQuery(sql);
-            while(output.next()){
-                Log.e("Output",output.getString(1));
-                Log.e("Output",output.getString(2));
-                message.add(output.getString(3));
-                Log.e("Output",output.getString(3));
-                Log.e("Output",output.getString(4));
-                // Log.e("Output",output.getString(5));
-                message_sender.add(userid_name.get(output.getString(2)));
-//                Log.e("Output",output.getString(6));
-                // names.add(output.getString(1));
-
-            }
-
-            this.runOnUiThread(new Runnable() {
-                public void run() {
-                    chat_adapter.notifyDataSetChanged();
-                }
-            });
-            connection.close();
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-            Log.e("Error",throwables.toString());
-        }
-
-
-            }
-
-
-
-
     public void setAdapter(){
 
         chat_adapter = new Chat_Adapter(Personal_Chats.this,message_sender,message);
