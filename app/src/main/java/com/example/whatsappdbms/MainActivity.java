c341e5ae44a787a378c08305d15f831f13ffe15a
Vishesh Jain <vishesh20550@iiitd.ac.in>
2022-04-27 00:53:49 +0530

    Login Credentials added

--- app/src/main/java/com/example/whatsappdbms/MainActivity.java
+++ app/src/main/java/com/example/whatsappdbms/MainActivity.java
@@ -2,16 +2,92 @@ package com.example.whatsappdbms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editTextPhone;
+    EditText editTextTextPassword;
+    String phoneNumberInput;
+    String passwordInput;
+    String userId;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextPhone =findViewById(R.id.editTextPhone);
+        editTextTextPassword = findViewById(R.id.editTextTextPassword);
+    }
+    public void loginClicked(View view) {
+        closeKeyBoard();
+        phoneNumberInput = editTextPhone.getText().toString();
+        passwordInput = editTextTextPassword.getText().toString();
+        if(phoneNumberInput.equals("") || passwordInput.equals(""))
+            Toast.makeText(this, "Please enter valid input", Toast.LENGTH_SHORT).show();
+        else{
+            String username = "root";
+            String password = "DBMSProject123";
+            Connection connection = null;
 
-        MainActivity.this.startActivity(new Intent(MainActivity.this, MainScreenActivity.class));
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+            StrictMode.setThreadPolicy(policy);
+            try {
+                Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            String ConnectionURL = "jdbc:mysql://103.252.116.130:3306/Whatsapp";
+            try {
+                connection = DriverManager.getConnection(ConnectionURL, username, password);
+                Log.e("Success", "Connection Successful");
+                Statement stmt = connection.createStatement();
+                String sql = "select user_id, password from auth_table where user_id =(select userid from userlist where phonenumber ="+phoneNumberInput+");";
+                ResultSet output = stmt.executeQuery(sql);
+                output.beforeFirst();
+                if(output.next()){
+                    if(!password.equals(output.getString(2))){
+                        Toast.makeText(this, "Wrong Password", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        userId = output.getString(1);
+                        intent = new Intent(this, MainScreenActivity.class);
+                        intent.putExtra("userId",userId);
+                        this.startActivity(intent);
+                    }
+                }
+                else{
+                    Toast.makeText(this, "No User Found", Toast.LENGTH_SHORT).show();
+                }
+                connection.close();
+            } catch (SQLException throwable) {
+                throwable.printStackTrace();
+                Log.e("Error", throwable.toString());
+            }
+        }
+    }
+    private void closeKeyBoard(){
+        View view = this.getCurrentFocus();
+        if (view != null){
+            InputMethodManager imm = (InputMethodManager)
+                    getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 }
--- app/src/main/java/com/example/whatsappdbms/MainScreenActivity.java
+++ app/src/main/java/com/example/whatsappdbms/MainScreenActivity.java
@@ -8,10 +8,14 @@ import android.view.View;
 
 public class MainScreenActivity extends AppCompatActivity {
     Intent intent;
+    Intent intentFromMain;
+    String userId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+        intentFromMain = getIntent();
+        userId = intentFromMain.getStringExtra("userId");
     }
     public void group (View view){
         intent =new Intent(MainScreenActivity.this, PersonalChatList.class);
